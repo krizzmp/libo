29787c830562ee71961a80007f3350d8177bb7cf
krizzmp <krizzmp@gmail.com>
2017-08-13 22:06:31 +0200

    other changes

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,13 +1,92 @@
 import * as React from "react";
-import styled from 'styled-components'
+import styled, { ThemedStyledFunction } from 'styled-components'
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import { RootState, actionCreators } from '../reducers'
+
+type bsp = { x: number, y: number }
 const BoxStyled = styled.div`
+    position: fixed;
     padding: 5px;
     border: 1px solid #f5f5f5;
+    left:${(p: bsp) => p.x}px;
+    top:${(p: bsp) => p.y}px;
+    user-select: none;
 `
-const Box = () => <BoxStyled>hello</BoxStyled>
+interface BoxProps {
+    x: number,
+    y: number,
+    selected: boolean,
+    id: string
+}
+class BoxView extends React.Component<any, BoxProps> {
+    componentDidUpdate(prevProps: BoxProps) {
+        if (prevProps.selected !== this.props.selected) {
+            if (this.props.selected) {
+                console.log(this.r);
+                this.r.focus();
+            }
+        }
+    }
+    private r: HTMLDivElement
+    mp(e: React.MouseEvent<HTMLDivElement>) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.props.onClick(this.props.id)
+    }
+    mip(e: React.FocusEvent<HTMLDivElement>) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.props.onClick('')
+    }
+    render() {
+        let props = this.props;
+        return (
+            <BoxStyled
+                x={props.x}
+                y={props.y}
+                contentEditable={props.selected}
+                onDoubleClick={(e) => this.mp(e)}
+                innerRef={(r) => this.r = r}
+                onBlur={(e) => this.mip(e)}
+            >
+                hello
+            </BoxStyled>
+        )
+    }
+}
+const Canvas = styled.div`
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+`
 
-export interface HelloProps { compiler: string; framework: string; }
-export const Hello = () =>
-    <div>
-        <Box />
-    </div>
+const mapStateToProps = (state: RootState) => ({
+    boxes: state.boxesList.map((id) => state.boxesById[id]),
+    selectedBox: state.selectedBox
+})
+const mapDispatch = (dispatch: Dispatch<RootState>) => ({
+    createBox(x: number, y: number) {
+        dispatch(actionCreators.createBox(x, y))
+    },
+    selectBox(id: string) {
+        dispatch(actionCreators.selectBox(id))
+    }
+})
+export const Hello = connect(mapStateToProps, mapDispatch)(
+    (props) =>
+        <Canvas onDoubleClick={(e) => props.createBox(e.clientX, e.clientY)}>
+            {props.boxes.map((box) =>
+                <BoxView
+                    key={box.id}
+                    selected={box.id === props.selectedBox}
+                    x={box.x}
+                    y={box.y}
+                    id={box.id}
+                    onClick={props.selectBox}
+                />
+            )}
+        </Canvas>
+)
--- src/reducers.tsx
+++ src/reducers.tsx
@@ -1,6 +1,69 @@
-interface Action<T extends string, P> {
+import { combineReducers } from 'redux'
+import cuid = require('cuid')
+
+type ById<T> = { [id: string]: T }
+
+const enum ActionType {
+    CreateBox,
+    SelectBox
+}
+interface Action<T extends ActionType, P> {
     type: T,
     payload: P
 }
-type Actions = Action<'test', {}>
-export const reducer = (state = {}, action: Actions) => (state)
+// Action Creators
+export const actionCreators = {
+    createBox: (x: number, y: number): Action<ActionType.CreateBox, { box: Box }> => ({
+        type: ActionType.CreateBox,
+        payload: { box: { id: cuid(), x, y } }
+    }),
+    selectBox: (id: string): Action<ActionType.SelectBox, { id: string }> => ({
+        type: ActionType.SelectBox,
+        payload: { id }
+    })
+}
+interface Box {
+    id: string,
+    x: number,
+    y: number
+}
+
+type Actions = Action<ActionType.CreateBox, { box: Box }> | Action<ActionType.SelectBox, { id: string }>
+type BoxesState = ById<Box>
+const boxesById = (state: BoxesState = {}, action: Actions): BoxesState => {
+    switch (action.type) {
+        case ActionType.CreateBox:
+            return { ...state, [action.payload.box.id]: { ...action.payload.box } };
+    }
+    return state
+}
+const boxesList = (state: string[] = [], action: Actions): string[] => {
+    switch (action.type) {
+        case ActionType.CreateBox:
+            return [...state, action.payload.box.id];
+    }
+    return state
+}
+type reducerType<T> = (state: T, action: Actions) => T
+function mr<T>(fn: (state: T, action: Actions) => T): (state: T, action: Actions) => T {
+    return fn
+}
+const selectedBox = mr<string>((state = '', action) => {
+    switch (action.type) {
+        case ActionType.SelectBox:
+            return action.payload.id;
+    }
+    return state
+}
+)
+
+export type RootState = {
+    boxesById: BoxesState,
+    boxesList: string[],
+    selectedBox: string
+};
+export const reducer = combineReducers({
+    boxesById,
+    boxesList,
+    selectedBox
+})
